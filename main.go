f550a73f71f745a71a513620e2b7c5a85dcc0f94
ppp <ppp.3ppj@gmail.com>
2024-01-29 23:29:14 +0700

    :tada: initial commit

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+)
+
+func main() {
+    app := fiber.New()
+
+    app.Use(logger.New())
+
+
+    app.Get("/", func(c *fiber.Ctx) error {
+        return c.SendString("Hello, World!")
+    })
+    log.Fatal(app.Listen(":1323"))
+}
